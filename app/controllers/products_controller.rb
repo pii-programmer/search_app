8888308da62ec3508eeffda2c2d44ca05fc949d1
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-25 16:30:22 +0900

    【コントローラー編集】検索条件を複数にする

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     set_product_column
+    set_category_column
   end
 
   def search
@@ -16,5 +17,11 @@ class ProductsController < ApplicationController
 
   def set_product_column
     @product_name = Product.select("name").distinct # distinctメソッドはDBからレコードを取得する際、重複したものを削除してくれる。
+    @product_size = Product.select("size").distinct
+    @product_status = Product.select("status").distinct
+  end
+
+  def set_category_column
+    @category_name = Category.select("name").distinct
   end
 end
